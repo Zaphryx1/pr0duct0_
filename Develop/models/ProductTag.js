84048adcf15b186a6d60a55281d326990b830e90
Zaphryx1 <michaele.gaytan@gmail.com>
2022-04-29 03:15:50 -0500

    filled in bp for ProductTag

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,29 +6,42 @@ class ProductTag extends Model {}
 
 ProductTag.init({
 
-    // define columns
+        // define columns
 
-    id: {
+        id: {
 
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        product_id: {
+
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'product',
+                key: 'id'
+            }
+        },
+
+        tag_id: {
+
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'product',
+                key: 'id'
+            }
+        },
 
     },
 
-    product_id: {
-
-
-    },
-
-    tag_id: {
-
-
-    },
-
-}, {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-});
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product_tag',
+    });
 
 module.exports = ProductTag;
